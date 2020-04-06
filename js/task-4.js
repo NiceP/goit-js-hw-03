67697c14a737a2424827953f02202883ed165c30
nicepraks <nicepraks@gmail.com>
2020-04-06 23:13:36 +0300

    thirdHomework

---
+++ js/task-4.js
@@ -0,0 +1,29 @@
+"use strict"
+const countTotalSalary = function(employees) {
+  let totalSalary = 0;
+  const salary = Object.values(employees);
+
+  for (const key of salary) {
+    totalSalary += key;
+  }
+  return totalSalary;
+};
+
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
